3723ade24d81990c4f812dadc42b75dee0e38a5e
Kevin Ndirangu <168788887+placcbo@users.noreply.github.com>
2025-11-15 13:42:37 +0000

    ...

---
+++ constants/main.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "Constant"
+
+func main(){
+	fmt.Println(s)
+	  const n = 50.00
+	    fmt.Println(int64(n))
+		a := 500
+		b := float64(a)
+		fmt.Println(math.Sin(b))
+}
