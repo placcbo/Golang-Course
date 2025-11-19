0c296342315124dc4b6ddea76b0b745775c17390
Kevin Ndirangu <168788887+placcbo@users.noreply.github.com>
2025-11-19 15:07:06 +0000

    ...

--- Twilio/main.go
+++ Twilio/main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main(){
-	messagesFromDoris := [] string{
-		"you doing anything later?",
-		"Did you get my last message",
-		"Don't leave me hunging..",
-		"please respond I'm lonely",
+	messagelength := 10
+
+	maxMessageLength := 20
+
+	fmt.Println("tyring to send a message of length:", messagelength)
+
+	if messagelength <maxMessageLength{
+		fmt.Println("Message sent")
+	} else{
+		fmt.Println("Message not sent")
 	}
-	numMessages := float64(len(messagesFromDoris))
-	costPerMessage := 0.02
-	totalCost := numMessages * costPerMessage
-	fmt.Println("you sent a total of", numMessages, "your total cost is",totalCost)
+
 }
